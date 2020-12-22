16f53649d50f6aaa927e47ed779731e15e0c4ee6
DarthProVader <73229155+DarthProVader@users.noreply.github.com>
2020-12-22 23:10:45 +0100

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -1,1 +1,67 @@
 
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+class Card():
+    
+    def __init__(self, suit, rank, value):
+        self.suit = suit
+        self.rank = rank
+        self.value = value[rank]
+    
+    def __str__(self):
+        return self.suit + "of" + self.rank
+
+class Deck:
+    
+    def __init__(self):
+        # Only happens once upon creation of a new Deck
+        self.all_cards = [] 
+        for suit in suits:
+            for rank in ranks:
+                # This assumes the Card class has already been defined!
+                self.all_cards.append(Card(suit,rank))
+                
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        return self.all_cards.pop()  
+
+class Player:
+    
+    def __init__(self,name):
+        self.name = name
+        # A new player has no cards
+        self.all_cards = [] 
+        
+    def remove_one(self):
+        # Note we remove one card from the list of all_cards
+        # We state 0 to remove from the "top" of the deck
+        # We'll imagine index -1 as the bottom of the deck
+        return self.all_cards.pop(0)
+    
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+dealer = Player("Dealer")
+player_name = input("Whats your name?: ")
+player = Player(player_name)
+
+new_deck = Deck()
+new_deck.shuffle()
+
+chips = 500
+
+bet = int(input("How much would you like to bet? Available chips:", chips, "\n"))
+# tomorrow
