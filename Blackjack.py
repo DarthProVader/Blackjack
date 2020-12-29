c186463e4591ddf093d48fc6f234711e1c7a13ad
DarthProVader <DarthProVader@users.noreply.github.com>
2020-12-29 18:29:18 +0100

    Almost finished game

--- Blackjack.py
+++ Blackjack.py
@@ -56,14 +56,14 @@ class Player:
     def __str__(self):
         return f'Player {self.name} has {len(self.all_cards)} cards.'
 
-def hand_value():
+def hand_value(player):
     result = 0
-    for i in range(len(gambler.all_cards)):
-        result += gambler.all_cards[i].value
+    for i in range(len(player.all_cards)):
+        result += player.all_cards[i].value
     return result
 
-def hit():
-    gambler.add_cards(new_deck.deal_one())
+def hit(player):
+    player.add_cards(new_deck.deal_one())
 
 chips = 500
 pot = 0
@@ -81,14 +81,62 @@ def bet():
 
 def decision():
     while True:
-        another_card = input("Would you like another card? (yes/no)\n")
-        if another_card.lower() == "yes":
-            hit()
-            print(hand_value())
-        elif another_card.lower() == "no":
-            return False
+        if hand_value(gambler) <= 21:
+            another_card = input("Would you like another card? (yes/no)\n")
+            if another_card.lower() == "yes":
+                hit(gambler)
+                print(gambler.all_cards[-1])
+                print(f"Hand value: {hand_value(gambler)}")
+            elif another_card.lower() == "no":
+                return False
+            else:
+                print("Incorrect input!")
         else:
-            print("Incorrect input!")
+            return True
+
+def dealer_turn():
+    dealer.all_cards = []
+    for i in range(2):
+        dealer.add_cards(new_deck.deal_one())
+    while hand_value(dealer) < 17:
+        hit(dealer)
+    return hand_value(dealer)
+
+def game_eval():
+    if hand_value(dealer) <= 21 and hand_value(gambler) <= 21:
+        if hand_value(dealer) > hand_value(gambler):
+            return 0
+        elif hand_value(dealer) < hand_value(gambler):
+            return 1
+        else:
+            return 2
+    elif hand_value(gambler) > 21:
+        return 0
+    else:
+        return 1
+
+def commentator(arg):
+    global pot
+    global chips
+    print("-"*7+"RESULT"+"-"*7)
+    if arg == 0:
+        print(f"Dealer's hand value: {hand_value(dealer)}\nYour hand value: {hand_value(gambler)}\n{dealer.name} wins.\nChips removed from pot: {pot}")
+        pot = 0
+    if arg == 1:
+        print(f"Dealer's hand value: {hand_value(dealer)}\nYour hand value: {hand_value(gambler)}\n{gambler.name} wins!\nChips won: {pot*2}")
+        chips += pot*2
+        pot = 0
+    if arg == 2:
+        print(f"Dealer's hand value: {hand_value(dealer)}.\nYour hand value: {hand_value(gambler)}.\nDraw.\nChips returned to you: {pot}")
+        chips += pot
+        pot = 0
+
+def hand_print(player):
+    print("-"*10+"HAND"+"-"*10)
+    print("Cards:")
+    for i in player.all_cards:
+        print(i)
+    print(f"Hand value: {hand_value(player)}")
 
 print("Welcome to BlackJack!")
 
@@ -99,6 +147,33 @@ gambler = Player(player_name)
 new_deck = Deck()
 new_deck.shuffle()
 
-chips = 500
-pot = 0
-# tomorrow
+game_on = True
+
+while game_on == True:
+    gambler.all_cards = []
+    print("-"*10+"BET"+"-"*10)
+    bet()
+    print(f"Pot: {pot}")
+    for i in range(2):
+        gambler.add_cards(new_deck.deal_one())
+
+    hand_print(gambler)
+    over21 = decision()
+
+    if over21 == False:
+        print("-"*7+"DEALER TURN"+"-"*6)
+        dealer_turn()
+        hand_print(dealer)
+
+        commentator(game_eval())
+    else:
+        print("-"*7+"RESULT"+"-"*7)
+        print(f"Your hand value: {hand_value(gambler)}\n{dealer.name} wins.\nChips removed from pot: {pot}")
+        pot = 0
+    
+    print("-"*7+"GAME ON?"+"-"*7)
+    print(f"Available chips: {chips}")
+    game_off = input("Press 1 if you wish to continue gambling.\nPress 2 if you wish to cash out.\n")
+    if game_off == "2":
+        game_on = False
+print(f"Here's your imaginary {chips} chips. Buy yourself something nice!")
